d2c72c98ef098e3f596e42eb36116f72ad86f8d6
mcgear <michael.gearhardt@gmail.com>
2023-12-13 11:24:42 -0700

    additional example

--- routes/api/octokit.ts
+++ routes/api/octokit.ts
@@ -2,3 +2,9 @@ import { Octokit } from "@octokit";
 import { paginateGraphql } from "@octokit/plugin-paginate-graphql";
 
 const TestOctokit = Octokit.plugin(paginateGraphql);
+
+const octokit = new TestOctokit({
+  auth: Deno.env.get("GITHUB_TOKEN"),
+});
+
+const user = octokit.rest.users.getAuthenticated();
